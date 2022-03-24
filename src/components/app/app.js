d1fb32ec8891a59a17c47bb45d717c1a291eb71e
Pavel <pavel.a.terekhov@gmail.com>
2022-03-24 09:01:20 +0700

    added component Advantages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,13 @@
 
 // import './App.css';
 import Promo from '../promo/promo';
+import Advantages from '../advantages/advantages';
 
 const App = () => {
   return (
       <div className="App">
         <Promo/>
+        <Advantages/>
       </div> 
   );
 }
