74dec3c96c3358c5d26e0f1977ed0d468272fc84
Pavel <pavel.a.terekhov@gmail.com>
2022-03-26 10:30:08 +0700

    Added component Consultation

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,12 +2,14 @@
 // import './App.css';
 import Promo from '../promo/promo';
 import Advantages from '../advantages/advantages';
+import Consultation from '../consultation/consultation';
 
 const App = () => {
   return (
       <div className="App">
         <Promo/>
         <Advantages/>
+        <Consultation/>
       </div> 
   );
 }
---
+++ src/components/consultation/consultation.js
@@ -0,0 +1,32 @@
+
+
+
+import './feed-form.scss';
+import '../buttons/_button.scss';
+import '../title/_title.scss';
+import './consultation.scss';
+
+
+const Consultation = () => {
+
+return (
+    <div className="consultation">
+    <div className="container">
+        <h2 className="title title_left">Получите бесплатную консультациюпо подбору пульсометра</h2>
+        <div className="consultation__descr">Просто заполните форму заявки и мы перезвоним вам в течении 10 минут</div>
+        <form className='feed-form' action="#">
+            <input required name='name' placeholder='Ваше имя' type="text" />
+            <input required name='number' placeholder='Ваш телефон' type="number" />
+            <input required name='email' placeholder='Ваш E-mail' type="email" />
+            <button className='button button_submit'>заказать кОНСУЛЬТАЦИЮ</button>
+        </form>
+    </div>
+</div>
+)
+
+}
+
+export default Consultation;
+
+
+
